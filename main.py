1b62db0ed58b8744d36f6d9a7c45e6748ae0ac42
mostafa-elhaiany <mostafa_elhaiany@outlook.com>
2020-05-18 18:42:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from Solver.data import *
 from Solver.models import *
+from glob import glob
+
+#setting random seed for training
 np.random.seed(42)
+
 #reading all the words
-words = read_data('data/TheStory.txt')
-words += read_data('data/AdamsElissa.txt')
-words += read_data('data/ch1.txt')
-words += read_data('data/ch2.txt')
+all_data = glob('data/*.txt')
+print(all_data)
+words=[]
+for path in all_data:
+    words += read_data(path)
+
+
 #getting the words in X,y numpys
 X,y = get_data(words)
 max_len=25
@@ -27,23 +34,25 @@ hangman_x,hangman_y,original_words=get_hangman_data(x,y)
 print(hangman_x.shape)
 
 
+#changing the letters to numerical values
 numerical_hangman_x = get_numerical_data(hangman_x)
 print(original_words[102])
 print(hangman_x[102])
 print(numerical_hangman_x[102])
 
-#setting y to be bounded by 0 1
-numerical_hangman_y=np.array([
-    Y/np.max(Y) if np.max(Y)>0 else Y for Y in hangman_y
-])
+#setting y to be bounded by 0 1 (sigmoid prediction)
+numerical_hangman_y= copy.deepcopy(hangman_y)
+numerical_hangman_y[numerical_hangman_y!=0]=1
+print(np.min(numerical_hangman_y),np.max(numerical_hangman_y))
 
 
-#loading model
+#loading a new model
 model = load_model(intput_size=numerical_hangman_x.shape[1])
 model.summary()
 
-## training the model
-save_best=keras.callbacks.ModelCheckpoint(f"models/model2.h5",
+
+#setting Callbacks
+save_best=keras.callbacks.ModelCheckpoint(f"models/model.h5",
                                                   monitor='val_loss', verbose=1,
                                                   save_best_only=True)
 
@@ -51,22 +60,23 @@ early_stop=keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0,
                                                   patience=20, verbose=1, mode='auto',
                                                   restore_best_weights=True)
 
+# training the model
 model.fit(numerical_hangman_x,numerical_hangman_y,
-        batch_size=128,epochs=10,verbose=1,
+        batch_size=128,epochs=100,verbose=1,
         validation_split=0.01,
         callbacks=[save_best,early_stop],
          shuffle=True)
  
 
 
-## loading and using the model
-# model = keras.models.load_model('models/model.h5')
-# i=random.randint(0,len(numerical_hangman_x)-1)
-# word = numerical_hangman_x[i].reshape((1,25,))
-# print('what the model sees: \n',word)
-# print('what that means: \n',hangman_x[i])
-# print('the actual word:  \n',original_words[i])
-# p=model.predict(word)
-# char = chr(np.argmax(p)+97)
-# print('predictions: \n',p)
-# print('final output: ',char)
+# loading and using the model
+model = keras.models.load_model('models/model.h5')
+i=random.randint(0,len(numerical_hangman_x)-1)
+word = numerical_hangman_x[i].reshape((1,25,))
+print('what the model sees: \n',word)
+print('what that means: \n',hangman_x[i])
+print('the actual word:  \n',original_words[i])
+p=model.predict(word)
+char = chr(np.argmax(p)+97)
+print('predictions: \n',p)
+print('final output: ',char)
